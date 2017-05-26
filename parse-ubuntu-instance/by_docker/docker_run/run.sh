534a0cccdcfa492c5892c63768e473029c760fc4
sqrt-avers <sqrt-avers@mail.ru>
2017-05-26 12:47:05 +0300

    new relise

--- parse-ubuntu-instance/by_docker/docker_run/run.sh
+++ parse-ubuntu-instance/by_docker/docker_run/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t server-part /parse-ubuntu-instance/by_docker/serv/Dockerfile
-docker build -t dashboard-part /parse-ubuntu-instance/by_docker/dash/Dockerfile
+docker build -t server-part ./parse-ubuntu-instance/by_docker/serv/Dockerfile
+docker build -t dashboard-part ./parse-ubuntu-instance/by_docker/dash/Dockerfile
 docker run -d -p 27017:27017 --link parse-server --name mongo mongo:latest
 docker run -d -p 1337:1337 --link mongo --name parse-server server-part
 docker run -d -p 4040:4040 --link parse-server --name parse-dashboard dashboard-part
