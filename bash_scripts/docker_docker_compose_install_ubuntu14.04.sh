d11b15f9ade0d97ffcfb970de27efa39096c3a25
sqrt-avers <sqrt-avers@mail.ru>
2018-01-17 12:58:19 +0200

    Update docker_docker_compose_install_ubuntu14.04.sh

--- bash_scripts/docker_docker_compose_install_ubuntu14.04.sh
+++ bash_scripts/docker_docker_compose_install_ubuntu14.04.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# install docker and docker-compose
 curl -sSL https://get.docker.com/ | sh
-curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-# curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-usermod -aG docker user
-
+usermod -aG docker $USER
+# create docker network
+docker network create -d bridge --subnet 192.168.0.0/24 --gateway 192.168.0.1 dockernet
