5d7a01992a16710f032f5ac472d7dc7c4f4a199d
sqrt-avers <sqrt-avers@mail.ru>
2018-04-15 00:00:50 +0300

    Update create_config-drive.sh

--- bash_scripts/create_config-drive.sh
+++ bash_scripts/create_config-drive.sh
@@ -15,3 +15,5 @@ EOF
 # Need libguestfs-tools pkg to be installed
 cd config-drive; LIBGUESTFS_BACKEND=direct virt-make-fs \
   --type=msdos --label=cidata . ../config-drive.img
+# laung an instance
+virt-install --import --name kub2 --ram 2048 --vcpus 1 --disk xenial-server-cloudimg-amd64-disk2.img,format=qcow2,bus=virtio --disk config-drive.img,device=cdrom --network default --os-type=linux --os-variant=ubuntu16.04 --noautoconsole
