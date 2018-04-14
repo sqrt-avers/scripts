6e1fce48a9e44e31b2da24f9a17a87a3fdb02f86
sqrt-avers <sqrt-avers@mail.ru>
2018-04-15 00:28:15 +0300

    Update create_config-drive.sh

--- bash_scripts/create_config-drive.sh
+++ bash_scripts/create_config-drive.sh
@@ -2,18 +2,28 @@
 mkdir -p config-drive
 cat > config-drive/user-data <<EOF
 #cloud-config
-ssh_authorized_keys:
- - $(cat ~/.ssh/id_rsa.pub)
+password: password
 chpasswd: { expire: False }
-password: pass
 ssh_pwauth: True
-EOF
+
+bootcmd:
+ - [ sh, -c, echo "=========bootcmd=========" ]
+
+runcmd:
+ - [ sh, -c, echo "=========runcmd=========" ]
+
+# add ssh public keys
+ssh_authorized_keys:
+  - $(cat ~/.ssh/id_rsa.pub)
+
+# This is for pexpect so that it knows when to log in and begin tests
+final_message: "SYSTEM READY TO LOG IN"
+
 cat > config-drive/meta-data <<EOF
 local-hostname: kube0
 EOF
 
-# Need libguestfs-tools pkg to be installed
-cd config-drive; LIBGUESTFS_BACKEND=direct virt-make-fs \
-  --type=msdos --label=cidata . ../config-drive.img
+genisoimage -output config-drive.iso -volid cidata -joliet -rock config-drive/user-data config-drive/meta-data
+
 # laung an instance
-virt-install --import --name kub2 --ram 2048 --vcpus 1 --disk xenial-server-cloudimg-amd64-disk2.img,format=qcow2,bus=virtio --disk config-drive.img,device=cdrom --network default --os-type=linux --os-variant=ubuntu16.04 --noautoconsole
+virt-install --import --name kub2 --ram 2048 --vcpus 1 --disk xenial-server-cloudimg-amd64-disk2.img,format=qcow2,bus=virtio --disk config-drive.iso,device=cdrom --network default --os-type=linux --os-variant=ubuntu16.04 --noautoconsole
