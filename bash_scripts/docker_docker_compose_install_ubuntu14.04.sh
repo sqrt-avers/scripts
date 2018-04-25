2ca4c93c1bbae471f7d059c58918bc54d652d87d
sqrt-avers <sqrt-avers@mail.ru>
2018-04-25 17:14:20 +0300

    Update docker_docker_compose_install_ubuntu14.04.sh

--- bash_scripts/docker_docker_compose_install_ubuntu14.04.sh
+++ bash_scripts/docker_docker_compose_install_ubuntu14.04.sh
@@ -2,7 +2,7 @@
 
 # install docker and docker-compose
 curl -sSL https://get.docker.com/ | sh
-curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 usermod -aG docker $USER
 # create docker network
